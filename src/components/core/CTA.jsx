79bfcefc41f15f1bcfd3da6949364e2a8d398b13
prajays <prajays.work@gmail.com>
2026-05-05 14:00:17 +0530

    Update CTA component

--- src/components/core/CTA.jsx
+++ src/components/core/CTA.jsx
@@ -8,7 +8,7 @@ const Cta = () => {
         className="text-sm break-words md:text-[16px] w-full p-8"
         aria-label="Tagline"
       >
-        Probably learning something new. <br/>ore wa monkey d luffy, <br className="md:hidden"/> kaizoku ou ni naru otoko da !!
+        Building things that matter. <br />Probably deploying at 2AM. <br className="md:hidden" /> Chasing the Alignment 🌱
       </MyButton>
     </div>
   );
