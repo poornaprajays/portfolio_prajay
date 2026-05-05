4d568fa65fe2ae7e942b512b99fbb102c9cebe49
prajays <prajays.work@gmail.com>
2026-05-05 13:57:35 +0530

    Update NameTag component and refine interests

--- src/components/common/NameTag.jsx
+++ src/components/common/NameTag.jsx
@@ -4,7 +4,7 @@ const NameTag = () => {
   return (
     <div className="flex flex-col">
       <div className="text-2xl font-bold flex gap-1 items-center md:text-[46px] md:-mt-3 sm:-mt-1">
-        Sanchit Vijay
+        Poorna Prajay S
       </div>
 
       <div className="-mt-3 flex gap-1 items-center md:-mt-4">
@@ -14,10 +14,12 @@ const NameTag = () => {
         <WordRotate
           className="text-sm text-white font-medium w-[160px] md:text-xl md:w-full"
           words={[
-            "a MERN Developer",
-            "a CP Coder",
+            "a Fullstack Developer",
+            "an AI/ML Engineer Intern",
             "a Tech Enthusiast",
-            "a Chasing the Alignment"
+            "Chasing the Alignment",
+            "a Restful sleep innovator",
+            "a Studio Ghibli adorer"
           ]}
         />
       </div>
