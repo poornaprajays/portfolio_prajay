4426efdccd36945218a75bf2b7015f76dc75dfa7
prajays <prajays.work@gmail.com>
2026-05-05 13:50:51 +0530

    Update education and experience details

--- src/components/constants/education.js
+++ src/components/constants/education.js
@@ -1,18 +1,18 @@
 const education = [
     {
-        name: "Ramaiah Institute of Technology",
-        location: "Bangalore, India",
+        name: "PES College of Engineering",
+        location: "Mandya, India",
         degree: "Bachelor of Engineering in Computer Science",
-        timeline: "2022-2026",
-        marks: "CGPA: 8.80"
+        timeline: "2023-2027",
+        marks: "CGPA: 8.23"
     },
     {
-        name: "Shemford Futuristic School",
-        location: "Uttar Pradesh, India",
-        degree: "Upper Secondary School",
-        timeline: "2020-2022",
-        marks: "Percentage: 88.8%"
+        name: "Cauvery Public School",
+        location: "Mandya, India",
+        degree: "Matriculation",
+        timeline: "2020-2021",
+        marks: "Percentage: 96%"
     }
 ]
 
-export {education}
+export { education }
--- src/components/constants/experience.js
+++ src/components/constants/experience.js
@@ -1,38 +1,16 @@
 const experience = [
     {
-        company: "Morgan Stanley",
-        location: "Bangalore, India",
-        role: "Apprentice",
-        link: "",
-        timeline: "Aug,2025 - Ongoing",
-        desc: [],
-        tech: ["Langchain", "Langgragh", "RAG"]
-    },
-    {
-        company: "HPE-CTY",
-        location: "Bangalore, India",
-        role: "Project Intern",
-        link: "https://bit.ly/hpe-cpp",
-        timeline: "Feb,2025 - July,2025",
+        company: "Anamtya Technologies Pvt. Ltd.",
+        location: "Bengaluru, India",
+        role: "Software Engineering Intern (AI/ML)",
+        link: "https://www.dropbox.com/scl/fi/eqwvmie4reradduuiahea/intership_prajay.jpeg?rlkey=q8sqiu7ckdj7d1pzskw53546l&st=tw88otqy&dl=0",
+        timeline: "Dec,2025 - Ongoing",
         desc: [
-            "Developing a gRPC server-client app in C++ to replace SOAP for efficient VASA communication.",
-            "Implementing secure HTTPS communication using x509 certificates with async request handling.",
-            "Building multithreaded solutions for resource sync and error management."
+            "Working on Project 'SonicWild', a professional production grade Electron application for 3D bio acoustic wildlife monitoring.",
+            "My role involves building frontend to managing remote device fleets via SSH and Node.js.",
+            "A central part of my work is the audio-to-insight pipeline: we capture live field recordings, visualize them through custom spectrograms, and use TensorFlow Lite classifiers to identify species, turning raw sound into actionable ecological data."
         ],
-        tech: ["gRPC", "HTTPS", "Envoy"]
-    },
-    {
-        company: "AamaraTech",
-        location: "Bangalore, India",
-        role: "Backend Development Intern",
-        link: "https://www.dropbox.com/scl/fi/ymfgsz6myncg2cld0mpiz/internship.jpg?rlkey=6pxjdttkfu9k93ecsh28tg9n6&st=6aw60ny4&dl=0s",
-        timeline: "June,2024 - Sept,2024",
-        desc: [
-            "Built scalable backend systems using Django with smooth frontend integration.",
-            "Optimized PostgreSQL databases for real-time data and complex queries.",
-            "Worked with cross-functional teams, following Agile to meet project goals."
-        ],
-        tech: ["Django", "PostgreSQL"]
+        tech: ["Electron", "Node.js/Express", "React18", "TypeScript", "SQLite3", "Socket.IO", "Three.js", "Leaflet"]
     }
 ]
 
